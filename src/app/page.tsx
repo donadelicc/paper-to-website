9974747defa25aefed7ac388c2500b4fc8a10771
donadelicc <preb1anders1@gmail.com>
2025-10-01 23:45:27 +0200

    V1.0: basic setup

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", minHeight: "100vh" }}>
+      <h1>Paper to Website</h1>
+      <p>Turn your research paper into a website effortlessly.</p>
+    </div>
+  );
+}
